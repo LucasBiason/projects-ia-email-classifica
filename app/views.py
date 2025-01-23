d13195cd55e2ee32ed9c8236e21e7c831a5b557a
Lucas Biason <biasonlucky@hotmail.com>
2025-01-23 18:37:02 -0300

    feat: colocando um tratamento para o case da model não treinada

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from .schemas import Message, PredictionResponse
 from .email_classifier import EmailClassifier
 
@@ -8,5 +8,8 @@ classifier = EmailClassifier()
 
 @router.post("/predict", response_model=PredictionResponse)
 def predict(message: Message):
-    prediction = classifier.predict(message.message)
-    return {"prediction": "spam" if prediction == 1 else "ham"}
+    try:
+        prediction = classifier.predict(message.message)
+        return {"prediction": "spam" if prediction == 1 else "ham"}
+    except FileNotFoundError as e:
+        raise HTTPException(status_code=500, detail=str(e))
